5be0846f698c749c028d2ce3d8a9e597fe7b563a
Владимир Free <kaifovovik@gmail.com>
2022-10-08 13:40:53 +0300

    Yes

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿ /*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Задача 10");
+Console.WriteLine("Введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
